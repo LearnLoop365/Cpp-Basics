e72208fbdb6eac010b7231c3f152393c44406bcf
br <silly.questioner@gmail.com>
2023-05-13 22:15:20 -0400

    Hello World

---
+++ 1_ Hello World/hw.cpp
@@ -0,0 +1,19 @@
+/*
+ * Build
+ * e.g.
+ * g++ hw.cpp -o build/hw
+ * clang++ hw.cpp -o build/hw
+ * c++ hw.cpp -o build/hw
+ *
+ * Run
+ * e.g.
+ * build/hw
+ * cd build; ./hw
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
